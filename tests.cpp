35eade0db44cdec54e8ab62db521a64f0c7939d1
chboo1 <samuel.navert@gmail.com>
2025-09-23 16:19:50 -0400

    I apparently modified tests.cpp since last time but forgot. Here it is.

--- tests.cpp
+++ tests.cpp
@@ -22,23 +22,33 @@ int main()
         Connection::closeListen();
         return 3;
     }
-    std::vector<Connection> acceptedStuff;
-    if (Connection::fetchConnections(&acceptedStuff) != 1 || acceptedStuff.size() != 1)
+    std::cout << "Wait start" << std::endl;
+    for (int i = 0; i < 1000000000; i++) {} // Waiting
+    std::cout << "Wait end" << std::endl;
+    std::vector<Connection*> acceptedStuff;
+    while (Connection::fetchConnections(&acceptedStuff) != 1 || acceptedStuff.size() < 1)
     {
         std::cerr << "Failed to accept connection." << std::endl;
-        Connection::closeListen();
-        return 4;
+        //Connection::closeListen();
+        //return 4;
     }
     char message[8] = "Hi me!\n";
     if (!conn.send(message, 7))
     {
         std::cerr << "Client failed to send data" << std::endl;
+        delete acceptedStuff[0];
         Connection::closeListen();
         return 5;
     }
-    if (acceptedStuff[0].readall() != "Hi me!\n")
+    std::cout << "Wait start" << std::endl;
+    for (int i = 0; i < 1000000000; i++) {} // Waiting
+    std::cout << "Wait end" << std::endl;
+    std::string data = acceptedStuff[0]->readall();
+    while (data != "Hi me!\n")
     {
-        std::cerr << "Server read the wrong data!" << std::endl;
+        std::cerr << "Server read the wrong data: `" << data << "'" << std::endl;
+        data = acceptedStuff[0]->readall();
     }
+    delete acceptedStuff[0];
     Connection::closeListen();
 }
