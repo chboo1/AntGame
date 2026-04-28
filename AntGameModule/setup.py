7348482a0c095866447204d48dba36d2b24f7d84
chboo1 <samuel.navert@gmail.com>
2026-04-28 14:20:27 -0400

    Added a compiler flag to use C++17 for python builds.

--- AntGameModule/setup.py
+++ AntGameModule/setup.py
@@ -9,6 +9,6 @@ setup(
         include_dirs=["src/headers"],
         define_macros=[("PYTHON_COMP","b")],
         sources=["src/AntGamemodule.cpp", "src/lib/map.cpp", "src/lib/network.cpp", "src/lib/sockets.cpp"],
-        extra_compile_args=["-Wno-sign-compare", "-Wno-unused-variable", "-Wno-unused-but-set-variable"]
+        extra_compile_args=["-Wno-sign-compare", "-Wno-unused-variable", "-Wno-unused-but-set-variable", "-std=c++17"]
     )],
 )
